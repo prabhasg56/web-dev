7a10eeb765739d395f8cb2afa8e073279964bfb4
Prabhakar Mandal <prabhask856@gmail.com>
2023-05-28 01:54:36 +0530

    getting doc properties

---
+++ DOM/dom.js
@@ -0,0 +1,13 @@
+console.log(document);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.domain);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
