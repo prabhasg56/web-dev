94ca21a01162983af81f9017f7fe445398cb632f
Prabhakar Mandal <prabhask856@gmail.com>
2023-05-28 03:01:38 +0530

    3rd item ininvisible

--- DOM/dom.js
+++ DOM/dom.js
@@ -121,7 +121,10 @@
 //// 2.Make the 3rd item invisible ////
 
 let secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green'
+secondItem.style.backgroundColor = 'green';
+
+let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden'
 
 
 
