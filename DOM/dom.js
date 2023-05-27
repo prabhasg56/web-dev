d0409334a833dd36999195e203713213264c5777
Prabhakar Mandal <prabhask856@gmail.com>
2023-05-28 01:56:14 +0530

    getElementById

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,13 +1,26 @@
-console.log(document);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.domain);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images);
+// console.log(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.domain);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// getElementById //
+
+console.log(document.getElementById('header-title'))
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'goodby';
+console.log(headerTitle.textContent);//Item Lister 123
+console.log(headerTitle.innerText);//Item Lister 
+headerTitle.innerHTML = '<h3>Hello 2</h3>';
+header.style.borderBottom = 'solid 3px #000'
 
