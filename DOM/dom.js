ac36c9c3f08981ac61898eaec7be9f009a596627
Prabhakar Mandal <prabhask856@gmail.com>
2023-05-28 01:58:02 +0530

    getElementByTagName

--- DOM/dom.js
+++ DOM/dom.js
@@ -26,13 +26,28 @@
 
 // getElementByClass//
 
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow' 
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow' 
 
-for(let i = 0; i<items.length; i++){
-    items[i].style.backgroundColor = 'lightgray';
+// for(let i = 0; i<items.length; i++){
+//     items[i].style.backgroundColor = 'lightgray';
+// }
+
+
+//getElementByTagName //
+
+let li = document.getElementsByTagName('li');
+console.log(li);
+console.log(items[1]);
+li[1].textContent = 'Hello';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow' 
+
+for(let i = 0; i<li.length; i++){
+    li[i].style.backgroundColor = 'lightgray';
 }
+
