ff00c9bbddebe0499c74c363d6b08356d6c561a4
Prabhakar Mandal <prabhask856@gmail.com>
2023-05-28 22:11:14 +0530

    remove item from the list

--- DOM/deleteRemoveFunDOM.js
+++ DOM/deleteRemoveFunDOM.js
@@ -1,8 +1,13 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items')
-
+let button = document.getElementsByClassName('delete');
 // Form submit event
 form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+// remove item from the list
+// Array.from(button).forEach((element,index) => {
+//     element.addEventListener('click', ()=>removeItem(index));
+// });
 
 function addItem(e){
     e.preventDefault();
@@ -27,11 +32,26 @@ function addItem(e){
 
     //Appen text node
     delBtn.appendChild(document.createTextNode('X'));
-    
+
     // Append to del button to li
     li.appendChild(delBtn);
 
     //append li to list
     itemList.appendChild(li);
+
+}
+
+function removeItem(e){
+    // let delBtn = document.createElement('button');
+    // let li = document.getElementsByClassName('list-group-item');
+    // li[index].remove();
+    // console.log(index);
+
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+          var li = e.target.parentElement;
+          itemList.removeChild(li);
+        }
+      }
 
 }
